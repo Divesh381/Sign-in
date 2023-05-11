82c3739e8d050424d30059c88e306bd128f9e237
Diveshkushwaha <132320851+Divesh381@users.noreply.github.com>
2023-05-11 19:56:47 +0530

    Add files via upload

---
+++ jpp.js
@@ -0,0 +1,14 @@
+// let main1= document.querySelector('.feature').getAttribute('class');
+// let main1= document.querySelector('.feature').attributes;
+// console.log(main1);
+
+
+let head=document.querySelector('header a');
+let a1=head.attributes[0].value;
+
+// a1.setAttribute('href','www.google.com')
+console.log(a1);
+console.log(head);
+
+
+
